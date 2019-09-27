e31589dca457e954525d2dabc58ddfb0475fd2f0
DiegoAscencio <is698355@iteso.mx>
2019-09-26 19:32:41 -0500

    Added viewAnimator in fragment_home.xml

---
+++ app/src/main/java/iteso/mx/tarea05/activities/HomePresenter.kt
@@ -0,0 +1,22 @@
+package iteso.mx.tarea05.activities
+
+
+class HomePresenter {
+    /**fun fetchInfo(objectId: String) {
+        view.showProgress()
+        doAsync {
+            Thread.sleep(1000)
+            val random = Random()
+            val randomInt = random.nextInt(20)
+            if (randomInt < 11) {
+                uiThread {
+                    view.showUser(User("Erick", 23))
+                }
+            } else {
+                uiThread {
+                    view.showNoUser()
+                }
+            }
+        }
+    }*/
+}
